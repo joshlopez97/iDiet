a71ce4f2653739bd5be1588cce56472f52d9854e
Qasim Shareh <qshareh@uci.edu>
2019-03-05 03:29:30 -0800

    QS: added some adjustments to skeleton and imported some set up code from mealsapi.js

--- user/fitbit.js
+++ user/fitbit.js
@@ -1,10 +1,35 @@
 (function(){
+
+  // connecting MYSQL database, taken from mealsapi.js code
+  const mysql = require('mysql');
+  const connection = mysql.createConnection({
+    host: 'idiet.cqywkz4otd3h.us-east-2.rds.amazonaws.com',
+    user: 'idiet',
+    password: '1a2b3c4d5e',
+    database: 'idiet'
+  });
+
+  // unirest needed for connection to fitbit api
+  const unirest = require('unirest');
+
+  // definition of fitbit for use in server.js
   function Fitbit(dependencies) {
     this.dependencies = dependencies;
     this.logged_in = false;
   }
 
+  // method for logging in a fitbit account
   Fitbit.prototype.login = function(username, password) {
+    // post for getting access token for a specified user
+    unirest.post("https://FitbitAPIdimashirokovV1.p.rapidapi.com/getAccessToken")
+    .header("X-RapidAPI-Key", "SIGN-UP-FOR-KEY")
+    .header("Content-Type", "application/x-www-form-urlencoded")
+    .send("clientId=USERNAME_WILL_GO_HERE")
+    .send("code=ACCESS_GRANT_CODE")
+    .send("clientSecret=PASSWORD_WILL_GO_HERE")
+    .end(function (result) {
+      console.log(result.status, result.headers, result.body);
+    });
     // IF LOGIN IS SUCCESSFUL
     // SET logged_in to true
     // ELSE IF LOGIN IS UNSUCCESSFUL
@@ -12,8 +37,9 @@
     return 0;
   };
 
-  Fitbit.prototype.calories = function() {
-    if (logged_in) {
+  // method for getting calories burnt for certain duration
+  Fitbit.prototype.calories = function(duration) {
+    if (this.logged_in) {
       // GET CALORIES
     }
     else {
@@ -22,9 +48,10 @@
     return 0;
   }
 
-  Fitbit.prototype.distance = function(interval) {
-    if (logged_in) {
-      // GET DISTANCE FOR GIVEN INTERVAL
+  // method for getting distance travelled for certain duration
+  Fitbit.prototype.distance = function(duration) {
+    if (this.logged_in) {
+      // GET DISTANCE FOR GIVEN duration
     }
     else {
       // RETURN SOME ERROR MESSAGE
